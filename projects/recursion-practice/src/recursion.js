092d7eff5496fa71b4b1d5da64edd03d90d4c132
royelWilliams <rjnw2014@gmail.com>
2022-10-18 18:12:03 +0000

    recrusion 2

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -142,13 +142,15 @@ var modulo = function(x, y) {
 // JavaScript's Math object.
 // ATTENTION DO NOT LEAVE COMMENTS IN THIS FUNCTION. The test is looking for any ('/').
 var multiply = function(x, y) {
-if(x==0 || y == 0){
+if(x == 0 || y == 0){
   return 0;
 }
-if(x == 1){
+else if(x == 1){
   return y;
 }
-return multiply(x- 1, y) + y
+else {
+return multiply(x- 1, y) + y;
+}
 };
 
 // 13. Write a function that divides two numbers without using the / operator  or
