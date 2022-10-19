e27e5f4de656ac0024acf2b0c2909e9fc3fdd354
royelWilliams <rjnw2014@gmail.com>
2022-10-19 23:02:18 +0000

    i

--- Studies/functions.js
+++ Studies/functions.js
@@ -1,4 +1,12 @@
 /**
  * FUNCTIONS 
- * A function is 
+ * A function when using javascript is very similar to a procedure it is made to deal with a set statement.
+ *  It then proforms task for that statement to calculate and return answers.When you are dealing with functions
+ * we have one or more inputs/ outputs.  To use a function, you must define it somewhere in the scope from which 
+ * you wish to call it. 
+ * you want to start by declaring a function 
+ * function 
+ * when declairing a function you need to name the function,
+ * function  nameHere 
+ * Next you want to
  */
