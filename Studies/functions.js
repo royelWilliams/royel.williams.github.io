6e7b69909ddd38131b1d1fb06b50750f4eddc454
royelWilliams <rjnw2014@gmail.com>
2022-10-20 01:01:45 +0000

    studies project

--- Studies/functions.js
+++ Studies/functions.js
@@ -5,8 +5,19 @@
  * we have one or more inputs/ outputs.  To use a function, you must define it somewhere in the scope from which 
  * you wish to call it. 
  * you want to start by declaring a function 
+ * ex.
  * function 
  * when declairing a function you need to name the function,
+ * ex.
  * function  nameHere 
- * Next you want to
- */
+ * Next you want to make a place for your parameters for your function 
+ * ex.
+ * function nameHere(parameter, parameter(this can generally hold as many parameters as needed))
+ * the next part of your function is the body: this is where the code generally goes
+ * ex 
+ * function nameHere(parameter,parameter){
+ * this is the body of the function 
+ * }
+ * following the function you want to impliment a function call 
+ * this is usually used to test a certain 
+ */ 
