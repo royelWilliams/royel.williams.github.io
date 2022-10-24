fb68652f12805b44f8ed1d390dc7c744ee114272
royelWilliams <rjnw2014@gmail.com>
2022-10-24 20:35:20 +0000

    chapter 4

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -1,8 +1,28 @@
 ////////////////////////////////////////////////////////////////////////////////
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+function range(start, end, step) {
+  // If no optional step, set step to 1.
+  if (step == null) step = 1;
 
-function range() {
+  // Initialize the array that will eventually be returned.
+  var array = [];
+
+  // If the range is increasing, continue pushing i to the    
+  // array and incrementing i by step until i is greater than 
+  // end, then stop.
+  if (step > 0) {
+    for (var i = start; i <= end; i += step)
+      array.push(i);
+  // If the range is decreasing, continue pushing i to the 
+  // array until i is less than end, then stop.
+  // Remember i will get smaller because step is negative.
+  } else {
+    for (var i = start; i >= end; i += step)
+      array.push(i);
+  }
+  // Return the result
+  return array;
 }
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
