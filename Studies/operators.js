b85d2b131f08772279ee2065ba20ff3688d41abe
royelWilliams <rjnw2014@gmail.com>
2022-10-25 02:54:46 +0000

    studies

--- Studies/operators.js
+++ Studies/operators.js
@@ -23,10 +23,19 @@
  * (**)- exponential:Raises a base number to the exponent power, that is, the base 
  * number multiplied by itself, exponent times.
  * COMPARISON OPERATORS:consist of different values that will in the end help determine if something is true or false in simple terms 
- * (===)- strictly equals 
- * (!==)-not equal
- * (<=)-less than or equal to 
- * (>=)-greater than or equal to 
- * (>)- greater than
- * (<)- less than 
+ * (===)- strictly equals:Tests whether the left and right values are identical to one another
+ * (!==)-not equal:Tests whether the left and right values are not identical to one another
+ * (<=)-less than or equal to: Tests whether the left value is smaller than or equal to the right one.
+ * (>=)-greater than or equal to :Tests whether the left value is greater than or equal to the right one.
+ * (>)- greater than:Tests whether the left value is greater than the right one.
+ * (<)- less than: Tests whether the left value is smaller than the right one.
+ * LOGICCAL OPERATORS 
+ * (&&)- and: for a set of boolean operands will be true if and only if all the operands are true
+ * (||)- or:for a set of operands is true if and only if one or more of its operands is true.
+ * (!)-not:the not operator is used when something is not true 
+ * UNARY OPERATORS:
+ * ()
+ * ()
+ * ()
+ * ()
  */
