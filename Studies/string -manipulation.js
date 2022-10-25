44e878a59fa1a7946081fab53caf96d7407e0fd4
royelWilliams <rjnw2014@gmail.com>
2022-10-25 12:58:30 +0000

    string manipulation

---
+++ Studies/string -manipulation.js
@@ -0,0 +1,30 @@
+/** 
+ * STRING-MANIPULATION
+ * string manipulation is when you have to go through a value of a string and change it up.
+ * Strings areIn JavaScript a string is not an array of characters but rather an object; and to change or
+ *  manipulate a string some functions or methods are required.
+ * we can manipulate a string by using operators or methods. 
+ * a quick example by using operators can be used simply as 
+ * var car = "red";
+ * var girl = "Ronny";
+ * var sentence =  girl + " drives a really fast " + car + " toyota.";
+ * this is using the plus operator to combine strings so the sentence would read 
+ * Ronny drives a really fast red toyota.
+ * strings can also be manipulated through various methods when using Javascript 
+ * this includes:
+ * concat(),- joins more than one string together without damaging or changing the original string 
+ *indexOf(), -It returns only the first occurrence of a word in a string
+ *lastIndexOf(), -method gives the index of the last occurrence of the specified word in the given string
+ *charAt(), - finds a character in a specific place in a string 
+ *match(), -This method searches the string to match expressions and returns the result as an Array  Object.
+ *replace(),- It searches the string for a specific value and then replaces it with the given value 
+ *split(), -It splits the string into an array of substrings and returns the new array.
+ *splice(),- It simply cuts a specific part of the given string and returns the isolated part. It takes either one or two parameters,
+ *length(), -returns the length of a given string (string length of a empty string is 0)
+ *substring(),-It takes two parameters, start and end, and returns the characters in these indexes  excluding the end character. 
+ *search(),- It searches a word in the string and returns its index. It returns -1 when no match is  found
+ *toUpperCase(),- uppercases the letters in a string 
+ *toLowerCase(),- lowercases the letters in a string 
+ *trim()-This method is used to remove all the whitespace characters (space, tab etc) from both sides of the string
+ * There are many methods that you can use on a string but these are some of the more commom methods that one would use to get through a code  
+ */
