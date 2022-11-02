367b6a3267bfd995cbee02aa8278d4e416120f40
royelWilliams <rjnw2014@gmail.com>
2022-11-02 02:09:15 +0000

    revisions3

--- Studies/operators.js
+++ Studies/operators.js
@@ -13,37 +13,53 @@
  *(%=)Remainder assignment	x %= f()	
  *(*=)Exponentiation assignment	x *= f()
  * these operators help to do simple math within a function o
+ * 
+
+
  * ARITHMETHIC OPERATORS: consist of the basic operators that we use to do sums 
- * (+)-addition: adds two numbers 
- * (-)-subtraction: subtracts two numbers
+ * (+)-addition: adds two numbers +"3" returns 3.
+ * (-)-subtraction: subtracts two numbers If x is 3, then -x returns -3.
  * (/)-division: divides two numbers 
  * (*)-multiplication:miltiplies two numbers 
- * (%)- remainder:Returns the remainder left over after you've divided the left number into a 
+ * (%)- remainder:Returns the remainder left over after you've divided the left number into a 12 % 5 returns 2.
  * number of integer portions equal to the right number.
- * (**)- exponential:Raises a base number to the exponent power, that is, the base 
+ * (**)- exponential:Raises a base number to the exponent power, that is, the base 2 ** 3 returns 8. 10 ** -1 returns 0.1.
  * number multiplied by itself, exponent times.
+ * 
+ * 
+ * 
  * COMPARISON OPERATORS:consist of different values that will in the end help determine if something is true or false in simple terms 
- * (===)- strictly equals:Tests whether the left and right values are identical to one another
- * (!==)-not equal:Tests whether the left and right values are not identical to one another
- * (<=)-less than or equal to: Tests whether the left value is smaller than or equal to the right one.
- * (>=)-greater than or equal to :Tests whether the left value is greater than or equal to the right one.
- * (>)- greater than:Tests whether the left value is greater than the right one.
- * (<)- less than: Tests whether the left value is smaller than the right one.
+ * (===)- strictly equals:Tests whether the left and right values are identical to one another  3 === var1
+ * (!==)-not equal:Tests whether the left and right values are not identical to one another 3 !== '3'
+ * (<=)-less than or equal to: Tests whether the left value is smaller than or equal to the right one. var1 <= 3
+ * (>=)-greater than or equal to :Tests whether the left value is greater than or equal to the right one.var1 >= 3
+ * (>)- greater than:Tests whether the left value is greater than the right one."12" > 2
+ * (<)- less than: Tests whether the left value is smaller than the right one."2" < 12
+ * 
+ * 
+ * 
  * LOGICCAL OPERATORS 
- * (&&)- and: for a set of boolean operands will be true if and only if all the operands are true
- * (||)- or:for a set of operands is true if and only if one or more of its operands is true.
- * (!)-not:the not operator is used when something is not true 
+ * (&&)- and: for a set of boolean operands will be true if and only if all the operands are true  expr1 && expr2
+ * (||)- or:for a set of operands is true if and only if one or more of its operands is true. expr1 || expr2
+ * (!)-not:the not operator is used when something is not true. !expr
+ * 
+
  * UNARY OPERATORS:
- * (+)-plus: tries to turn a number to a positive 
- * (++)-incriment:	Adds one to its operand
- * (-)-minus: tried to turn a number to a negative 
- * (--)-decriment:Decrements by one from its operand
+ * (+)-plus: tries to turn a number to a positive +"3" returns 3. +true returns 1
+ * (++)-incriment:	Adds one to its operand. If x is 3, then ++x sets x to 4 and returns 4, whereas x++ returns 3 and, only then, sets x to 4.
+ * (-)-minus: tried to turn a number to a negative If x is 3, then -x returns -3.
+ * (--)-decriment:Decrements by one from its operand If x is 3, then --x sets x to 2 and returns 2, whereas x-- returns 3 and, only then, sets x to 2.
  * (!)-not:Converts to boolean value then negates it(this can change any value/ statement from false to true or vice versa)
  * (typeof)-type of: Returns a string which is the type of the operand(this works with all simple data types it gets more complicated with objects, arrays, etc)
  * (delete)-delete:Deletesspecific index of an array or specific property of an object
- * TERNARY OPERATORS:
+ * 
+ * 
+ * 
+ * TERNARY OPERATORS
  * this is often  only JavaScript operator that takes three operands: a condition followed by a question mark (?),
  *  then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy. 
  * (a(being the expression or thing that needs to be solved)? b(being true):c(being false))
  * (a?b:c
+ * 
+ * condition ? exprIfTrue : exprIfFalse
  */
